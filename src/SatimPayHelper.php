645e988da8988e822f64fbcf0d579d7cc78ee446
n4ss1m <nassim0x09@gmail.com>
2024-12-20 02:51:23 +0100

    Refactor SatimPayHelper for improved data handling clarity
    Renamed internal variables and enhanced method documentation for better clarity and consistency in handling order response data. Added detailed comments and updated logic to streamline the retrieval and processing of response data, ensuring robust error handling.

--- src/SatimPayHelper.php
+++ src/SatimPayHelper.php
@@ -7,57 +7,82 @@ use PiteurStudio\Exception\SatimMissingDataException;
 
 trait SatimPayHelper
 {
-    private ?array $registerPaymentData = null;
+    private ?array $registerOrderResponse = null;
 
     /**
-     * Get payment data from generated payment
+     * Retrieves the response data for the registered order.
      *
-     * @throws SatimMissingDataException
+     * This method retrieves the response data associated with the registered order.
+     * The response data is stored when the registerOrder() method is called.
+     * If registerOrder() was not called before calling this method, a SatimMissingDataException is thrown.
+     *
+     * @return array The response data associated with the registered order.
+     * @throws SatimMissingDataException If the registerOrder() was not called first.
      */
     public function getResponse(): array
     {
-        if (empty($this->registerPaymentData)) {
-            throw new SatimMissingDataException('No payment data found. Call registerOrder() first.');
+        if (empty($this->registerOrderResponse)) {
+            throw new SatimMissingDataException(
+                'No payment data found. Call registerOrder() first to register the order and obtain the response data.'
+            );
         }
 
-        return $this->registerPaymentData;
+        return $this->registerOrderResponse;
     }
 
-    /*
-     * Get order id from generated payment
-     * @return int
-     * */
     /**
-     * @throws SatimMissingDataException
+     * Retrieves the order ID from the response data.
+     *
+     * This method extracts the order ID from the response data obtained from the Satim API.
+     * It relies on the getResponse() method to retrieve the response data.
+     *
+     * @return int The order ID extracted from the response data.
+     * @throws SatimMissingDataException If the registerOrder() was not called first.
      */
     public function getOrderId(): int
     {
-        return $this->getResponse()['orderId'];
+        // Retrieve the response data from the getResponse() method
+        $responseData = $this->getResponse();
+
+        // Extract the order ID from the response data
+        return $responseData['orderId'];
     }
 
-    /*
-     * Redirect to payment page
-     * @return void
-     * */
     /**
-     * @throws SatimMissingDataException
+     * Redirects the user to the URL specified in the response data.
+     *
+     * This method sends an HTTP header to perform the redirection and terminates the script execution.
+     * This method should be used after the registerOrder() method has been called to redirect the user to the
+     * payment form.
+     *
+     * @return void
+     * @throws SatimMissingDataException If the registerOrder() was not called first.
      */
     #[NoReturn]
     public function redirect(): void
     {
-        header('Location: '.$this->getResponse()['formUrl']);
+        // Retrieve the URL from the getResponse() method
+        $url = $this->getUrl();
+
+        // Set the HTTP header for redirection
+        header('Location: '.$url);
+
+        // Terminate the script execution to prevent any further code execution
         exit;
     }
 
-    /*
-     * Get payment url
-     * @return string
-     * */
     /**
-     * @throws SatimMissingDataException
+     * Retrieves the URL of the payment form from the response data.
+     *
+     * @return string The URL of the payment form associated with the response data.
+     * @throws SatimMissingDataException If the registerOrder() was not called first.
      */
     public function getUrl(): string
     {
-        return $this->getResponse()['formUrl'];
+        // Retrieve the response data from the getResponse() method
+        $responseData = $this->getResponse();
+
+        // Extract the URL from the response data
+        return $responseData['formUrl'];
     }
 }
